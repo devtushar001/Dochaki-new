15de2581ab4410b7d186134be8edb1a6e355fd6e
devtushar001 <tc341269@gmail.com>
2025-02-03 02:15:11 -0500

    Product shows successfully

--- client/src/Component/CategoryBox/CategoryBox.jsx
+++ client/src/Component/CategoryBox/CategoryBox.jsx
@@ -1,26 +1,29 @@
 import React, { useContext, useEffect } from "react";
 import { DochakiContext } from "../../Context/DochakiContext";
+import './CategoryBox.css';
 
 const CategoryBox = () => {
-    const {categories} = useContext(DochakiContext)
+   const { categories } = useContext(DochakiContext)
 
-    useEffect(() =>  {
-        console.log(categories)
-    })
-     return (
-        <>
-          <div className="single-category">
-             {categories.map((item, i) => {
-                return (
-                    <div id="cat-box" key={i}>
-                       <img src={item.img} alt="" />
-                       <p>{item.name}</p>
-                    </div>
-                )
-             })}
-          </div>
-        </>
-     )
+   useEffect(() => {
+      console.log(categories)
+   })
+   return (
+      <>
+         <div className="single-category">
+            {categories.map((item, i) => {
+               return (
+                  <div id="cat-box" key={i}>
+                     <img src={item.img} alt="" />
+                     <div className="name">
+                        <p>{item.name}</p>
+                     </div>
+                  </div>
+               )
+            })}
+         </div>
+      </>
+   )
 }
 
 export default CategoryBox;
