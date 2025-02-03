4b6ed735556a37b231e13edd650391e9688b8699
devtushar001 <tc341269@gmail.com>
2025-02-03 08:06:37 -0500

    all done

--- admin/src/Components/TextEditor/TextEditor.jsx
+++ admin/src/Components/TextEditor/TextEditor.jsx
@@ -4,18 +4,16 @@ import { DochakiContext } from '../../Context/DochakiContext';
 
 const TextEditor = () => {
     const editor = useRef(null);
-    const {content, setContent} = useContext(DochakiContext);
-    
-
-   
+    const { content, setContent } = useContext(DochakiContext);
 
     return (
-        <JoditEditor
-        style={{fontFamily: "Arial"}}
-            ref={editor}
-            value={content}
-            onBlur={newContent => setContent(newContent)} // Updates content on blur
-        />
+        <div style={{fontFamily: 'Arial', height: "65vh"}}>
+            <JoditEditor
+                ref={editor}
+                value={content}
+                onBlur={newContent => setContent(newContent)} // Updates content on blur
+            />
+        </div>
     );
 };
 
--- admin/src/Pages/AddItem/AddItem.jsx
+++ admin/src/Pages/AddItem/AddItem.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import ImageUploader from "../../Components/ImageUploader/ImageUploader";
+import TextEditor from "../../Components/TextEditor/TextEditor";
+import { DochakiContext } from "../../Context/DochakiContext";
+import './AddItem.css';
 
 const AddItem = () => {
+    const { content, imageData } = useContext(DochakiContext);
+
     return (
         <>
-        <h1>Add item</h1>
+            <div style={{ display: "flex", alignItems: "center", width: "100vw", flexDirection: "column" }} className="add-item">
+                <ImageUploader />
+                <div className="show-image-data">
+                    {imageData.map((item, i) => {
+                        return (
+                            <div className="single-img" key={i}>
+                                <img src={item} alt="" />
+                                <p>{item}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div style={{ display: "flex", alignItems: "center", width: "100vw", flexDirection: "column" }} className="main-content">
+                    <div style={{ width: "80vw" }} className="content-editor">
+                        <TextEditor />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
